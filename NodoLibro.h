b12ee959e20098fd5af0c8f86302f7d1eb368772
Victor Segura <seguravictor57@gmail.com>
2021-04-24 18:39:17 -0600

    implementacion de clase NodoLibro

--- NodoLibro.h
+++ NodoLibro.h
@@ -1,1 +1,39 @@
 #pragma once
+#ifndef NodoLibro_H
+#define NodoLibro_H
+#include <iostream>
+#include <sstream>
+#include "Libro.h"
+using std::string;
+using std::stringstream;
+using std::endl;
+using std::cout;
+using std::cin;
+
+class NodoLibro {
+
+private:
+
+	Libro* dato;
+	NodoLibro* sigDato;
+
+public:
+
+	NodoLibro(Libro*);
+	~NodoLibro();
+
+	// sets
+
+	void setLibro(Libro*);
+	void setSig(NodoLibro*);
+
+	// gets
+
+	Libro* getLibro();
+	NodoLibro* getSig();
+
+};
+
+#endif // !NodoLibro_
+
+
