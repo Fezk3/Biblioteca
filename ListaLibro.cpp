5bc99efecd29895dd57a29996e09e9905a1909c7
Victor Segura <seguravictor57@gmail.com>
2021-04-24 19:38:15 -0600

    implementacion de clase ListaLibro

---
+++ ListaLibro.cpp
@@ -0,0 +1,129 @@
+#include "ListaLibro.h"
+
+ListaLibro::ListaLibro(): head(NULL) {
+
+}
+
+ListaLibro::~ListaLibro() {
+
+	NodoLibro* borrar;
+
+	while (head != NULL)
+	{
+		borrar = head;
+		head = head->getSig();
+		borrar->setSig(NULL);
+		delete borrar;
+	}
+
+}
+
+void ListaLibro::agregarLibro(Libro* libro) {
+
+	NodoLibro* aux = new NodoLibro(libro);
+
+	if (head == NULL) {
+		head = aux;
+	}
+	else
+	{
+		aux->setSig(head);
+		head = aux;
+	}
+
+}
+
+void ListaLibro::EliminarLibro() {
+
+	// agragar id
+	string id;
+
+	NodoLibro* aux = head;
+	NodoLibro* aux2;
+
+	if (aux == NULL) {
+		cout << "El libro no existe" << endl;
+	}
+	if (aux->getLibro()->getId() == id) {
+		aux2 = aux->getSig();
+		delete aux;
+		head = aux2;
+	}
+	else
+	{
+		while (aux != NULL) {
+
+			if (aux->getLibro()->getId() == id) {
+
+				aux->setSig(aux->getSig()->getSig());
+
+			}
+
+			aux = aux->getSig();
+
+		}
+	}
+
+}
+
+void ListaLibro::mostrarColeccion() {
+
+	NodoLibro* aux = head;
+
+	if (aux == NULL) {
+
+		cout << "Lista vacia :(" << endl;
+
+	}
+
+	while (aux != NULL) {
+
+		cout<< aux->getLibro()->tostring()<<endl;
+		aux = aux->getSig();
+
+	}
+
+}
+
+bool ListaLibro::buscarLibro(string buscado) {
+
+	NodoLibro* aux = head;
+
+	while (aux != NULL) {
+
+		if (buscado == aux->getLibro()->getId() || buscado == aux->getLibro()->getAutores() || buscado == aux->getLibro()->getTitulo()) {
+
+			return true;
+
+		}
+
+	}
+
+	return false;
+
+}
+
+void ListaLibro::mostrarLibro() {
+
+	string id;
+	NodoLibro* aux = head;
+
+	if (buscarLibro(id)) {
+
+		while (aux != NULL) {
+
+			if (id == aux->getLibro()->getId()) {
+
+				cout << aux->getLibro()->tostring();
+
+			}
+
+		}
+
+	}
+	else
+	{
+		cout << "El libro no pertenece a la coleccion" << endl;
+	}
+
+}
---
+++ ListaLibro.h
@@ -0,0 +1,34 @@
+#pragma once
+#ifndef ListaLibro_H
+#define ListaLibro_H
+#include "NodoLibro.h"
+#include <iostream>
+#include <sstream>
+using std::string;
+using std::stringstream;
+using std::endl;
+using std::cout;
+using std::cin;
+
+class ListaLibro {
+
+private:
+
+	NodoLibro* head;
+
+public:
+
+	ListaLibro();
+	~ListaLibro();
+
+	// methods
+
+	virtual void agregarLibro(Libro*);
+	virtual void EliminarLibro();
+	virtual void mostrarLibro();
+	virtual bool buscarLibro(string);
+	virtual void mostrarColeccion();
+
+};
+
+#endif // !ListaLibro_H
